68b34189967712cc8ba80ba17418a1c8c50bb834
kaipak <kai@kaipak.org>
2018-04-07 22:44:45 +0000

    Removed system-wide upgrade.

--- bin/pangeo_gcs_setup.sh
+++ bin/pangeo_gcs_setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+# Script to get GCS Jupyter pod in a state that can reliably run GCS Cloud management
+# tools and run ASV benchmarks.
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install lsb-release
+sudo apt update && sudo apt -y install lsb-release
 
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt -y install google-cloud-sdk
+sudo apt update && sudo apt -y install google-cloud-sdk
 
-conda update conda
-
-export PATH=$PATH:~/ldeo-toolkit/bin:~/.local/bin/
+conda update --yes conda
