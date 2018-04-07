ba4dee334b5cf5be95664d695bb95b92ec4502ba
kaipak <kai@kaipak.org>
2018-04-07 00:25:47 +0000

    Add script to setup GCS worker with gcs tools

---
+++ bin/pangeo_gcs_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install lsb-release
+
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt -y install google-cloud-sdk
