8a2e9e80791e45014202cb117b703f463662dd0a
kaipak <kai@kaipak.org>
2018-04-07 00:52:13 +0000

    Added updates to PATH as required.

--- bin/pangeo_gcs_setup.sh
+++ bin/pangeo_gcs_setup.sh
@@ -8,3 +8,5 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt -y install google-cloud-sdk
+
+export PATH=$PATH:~/ldeo-toolkit/bin:~/.local/bin/
