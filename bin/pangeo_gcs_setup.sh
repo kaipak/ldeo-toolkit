ef8d2045de6fcf94c083d15a1498dc782346a86a
kaipak <kai@kaipak.org>
2018-04-07 04:31:31 +0000

    Update conda environment as well.

--- bin/pangeo_gcs_setup.sh
+++ bin/pangeo_gcs_setup.sh
@@ -9,4 +9,6 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt -y install google-cloud-sdk
 
+conda update conda
+
 export PATH=$PATH:~/ldeo-toolkit/bin:~/.local/bin/
