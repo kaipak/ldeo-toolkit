5fb78610339ffb97cd5fb0d924bf9b6ee16082c1
kaipak <kai@kaipak.org>
2018-04-08 15:52:39 +0000

    Added apt install less

--- bin/pangeo_gcs_setup.sh
+++ bin/pangeo_gcs_setup.sh
@@ -2,7 +2,7 @@
 # Script to get GCS Jupyter pod in a state that can reliably run GCS Cloud management
 # tools and run ASV benchmarks.
 
-sudo apt update && sudo apt -y install lsb-release
+sudo apt update && sudo apt -y install lsb-release ssh-client less
 
 export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -13,6 +13,6 @@ echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /e
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt update && sudo apt -y install google-cloud-sdk gcsfuse
 
-
-
 conda update --yes conda
+
+chmod 0600 ~/.ssh/id_rsa
